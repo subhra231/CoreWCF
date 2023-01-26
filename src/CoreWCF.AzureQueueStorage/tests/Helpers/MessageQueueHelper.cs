33d89a632413c76cc4b7a23462b0cb5bbfb01e09
subhra231 <subhra.nits@gmail.com>
2023-01-25 23:49:36 -0800

    Adding initial tests for Azure Queue Storage

---
+++ src/CoreWCF.AzureQueueStorage/tests/Helpers/MessageQueueHelper.cs
@@ -0,0 +1,19 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System.Diagnostics;
+using System.IO;
+using System.Threading.Tasks;
+using Azure.Messaging;
+using Azure.Storage.Queues;
+
+namespace CoreWCF.AzureQueueStorage.Tests.Helpers
+{
+    internal class MessageQueueHelper
+    {
+        public static void SendMessageInQueue(string queueName)
+        {
+
+        }
+    }
+}
